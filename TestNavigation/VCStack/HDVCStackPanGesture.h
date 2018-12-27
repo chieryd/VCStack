c6ac6565127d01529dcd0367fb666c9575ce1f78
chieryw <chieryw@163.com>
2018-12-27 15:01:00 +0800

    first commit

---
+++ TestNavigation/VCStack/HDVCStackPanGesture.h
@@ -0,0 +1,17 @@
+//
+//  HDVCStackPanGesture.h
+//  TestNavigation
+//
+//  Created by HanDong Wang on 2018/12/17.
+//  Copyright © 2018 HanDong Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HDVCStackPanGesture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
